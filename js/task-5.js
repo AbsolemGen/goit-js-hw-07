1ec808a932de5f6847536a642e3666f4ebbe6d45
Fayzillo Khamraev <absolemgen@gmail.com>
2024-01-22 20:27:05 +0100

    reset

---
+++ js/task-5.js
@@ -0,0 +1,18 @@
+const options = [
+  { label: 'червоний', color: '#F44336' },
+  { label: 'зелений', color: '#4CAF50' },
+  { label: 'синій', color: '#2196F3' },
+  { label: 'сірий', color: '#607D8B' },
+  { label: 'рожевий', color: '#E91E63' },
+  { label: 'індіго', color: '#3F51B5' },
+];
+const body = document.querySelector('body');
+const btnColorBody = document.querySelector('.change-color');
+const textColorName = document.querySelector('span');
+btnColorBody.addEventListener('click', onBtnColorChange);
+function onBtnColorChange() {
+  const randomColor = Math.floor(Math.random() * options.length);
+  const selectColor = options[randomColor];
+  textColorName.textContent = selectColor.label;
+  document.body.style.backgroundColor = selectColor.color;
+}
